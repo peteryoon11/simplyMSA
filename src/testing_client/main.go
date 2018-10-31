4f86a77d363a640a69d3ff65d7a68bcd8da501ee
peteryoon11 <yjisub11@gmail.com>
2018-10-31 16:21:42 +0900

    make testing client proto

--- src/testing_client/main.go
+++ src/testing_client/main.go
@@ -2,13 +2,61 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"time"
+
+	"github.com/kardianos/service"
 )
 
 var exitChain <-chan time.Time
 
-func main() {
+type program struct {
+}
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+func (p *program) run() {
 	daemonmain()
+}
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	exitChain = time.NewTimer(time.Second * 2).C
+	<-time.After(time.Second * 9)
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "msa testing client",
+		DisplayName: "msa testing client",
+		Description: "msa testing client",
+	}
+
+	prg := &program{}
+
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	if len(os.Args) > 1 {
+
+		if strings.EqualFold(os.Args[1], "-V") {
+			fmt.Println("")
+			return
+		}
+
+	}
+
+	err = s.Run()
+	if err != nil {
+		fmt.Println(err)
+
+	}
 }
 
 func daemonmain() {
@@ -33,11 +81,11 @@ func daemonmain() {
 
 	logRotateTimer = time.Second * time.Duration(logrotate_seconds)
 	logRotateChan = time.NewTimer(logRotateTimer).C
-	worker5minTimer = time.Second * time.Duration(30)
+	worker5minTimer = time.Second * time.Duration(5)
 
 	fmt.Println("  Interval timer = ", worker5minTimer)
 	worker5minChan = time.NewTimer(worker5minTimer).C
-
+	module_run = true
 	for {
 		if true == exit || false == module_run {
 			break
