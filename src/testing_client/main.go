608a706c33db1c4ce8a8bc4a64171b06e1e113fa
peteryoon11 <yjisub11@gmail.com>
2018-11-01 15:53:51 +0900

    remove part of code \n test objec

--- src/testing_client/main.go
+++ src/testing_client/main.go
@@ -63,45 +63,27 @@ func daemonmain() {
 	var (
 		module_run bool = false
 		//	err               error
-		logRotateChan     <-chan time.Time
-		logRotateTimer    time.Duration
-		logrotate_seconds int64 = 0
 
-		worker5minTimer time.Duration
-		worker5minChan  <-chan time.Time
+		IntervalValue     time.Duration
+		IntervalValueChan <-chan time.Time
 	)
 
 	var exit bool = false
 
-	var logrotate_interval int64 = 86400 // 하루 단위로
-	_, time_offset := time.Now().Zone()
-	logrotate_seconds = (time.Now().Unix() + int64(time_offset)) % logrotate_interval
-	logrotate_seconds = logrotate_interval - logrotate_seconds
-	logrotate_seconds += 1
+	IntervalValue = time.Second * time.Duration(5)
 
-	logRotateTimer = time.Second * time.Duration(logrotate_seconds)
-	logRotateChan = time.NewTimer(logRotateTimer).C
-	worker5minTimer = time.Second * time.Duration(5)
-
-	fmt.Println("  Interval timer = ", worker5minTimer)
-	worker5minChan = time.NewTimer(worker5minTimer).C
+	fmt.Println("  Interval timer = ", IntervalValue)
+	IntervalValueChan = time.NewTimer(IntervalValue).C
 	module_run = true
 	for {
 		if true == exit || false == module_run {
 			break
 		}
 		select {
-		case <-logRotateChan:
-			//	report_worker_logger.RotateLogFiles()
-			logrotate_seconds = (time.Now().Unix() + int64(time_offset)) % logrotate_interval
-			logrotate_seconds = logrotate_interval - logrotate_seconds
-			logrotate_seconds += 1
-			logRotateTimer = time.Second * time.Duration(logrotate_seconds)
-			logRotateChan = time.NewTimer(logRotateTimer).C
-			break
-		case <-worker5minChan:
 
-			worker5minChan = time.NewTimer(worker5minTimer).C
+		case <-IntervalValueChan:
+
+			IntervalValueChan = time.NewTimer(IntervalValue).C
 			fmt.Println("now called")
 			break
 
